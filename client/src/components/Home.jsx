80ea40f656d39b65a12b9dca667d8922adac755a
abdallah0128318 <boda0128318@gmail.com>
2024-08-07 03:02:21 +0300

    added some styles

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='bg-gray-600	 md:max-xl:bg-red-700 xl:bg-yellow-500  h-11'>Home</div>
   )
 }
