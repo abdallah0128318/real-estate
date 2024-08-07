0f3783aaac65822dab691e55a3c690891ef88fca
abdallah0128318 <boda0128318@gmail.com>
2024-08-07 03:33:14 +0300

    added pdf viewer to our app

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
+// pdfjs uses a web worker to process the most tasks which take time such as parsing and rendering a PDF document. 
+// The web worker is loaded via the `workerUrl` parameter
+// import the worker and viewer component
+import { Worker, Viewer } from '@react-pdf-viewer/core';
+// import viewer styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
 
-export default function Home() {
+function Home() {
+  const [pdfFile, setPdfFile] = useState("./Abdallah-Qapeel-Taha.pdf")
   return (
-    <div className='bg-gray-600	 md:max-xl:bg-red-700 xl:bg-yellow-500  h-11'>Home</div>
-  )
+    <>
+    <h1 className='text-4xl font-serif font-bold mt-11 text-center'>Pdf Audio Async</h1>
+    <div className='mt-10'>
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+        <Viewer fileUrl={pdfFile} />
+      </Worker>
+    </div>
+    </>
+)
 }
+
+export default Home
